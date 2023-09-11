2ffc3474c92cee52d5e99fd0ace354ab3fd5941c
TheMartynasXS <Marcius922@gmail.com>
2023-09-11 21:29:22 +0300

    Delete .svelte-kit directory

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.d92fc165.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.0719bd3d.js","_app/immutable/chunks/singletons.43e61cfd.js"];
-export const stylesheets = [];
-export const fonts = [];
